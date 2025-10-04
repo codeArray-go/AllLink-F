6d6707985d80b954a07762712200c4293fd26ff8
Akhil singh Bhandari <loneranger2520@gmail.com>
2025-10-04 13:58:41 +0530

    Update Navbar.js
    Dark and light theme toggle button is added also Theme color is changed.

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,6 +1,6 @@
-"use client"
+"use client";
 
-import { useEffect} from 'react';
+import { useEffect } from 'react';
 import Link from 'next/link';
 import { usePathname, useRouter } from 'next/navigation';
 import useAuthStore from '@/store/authStore';
@@ -8,15 +8,17 @@ import Button from './Button';
 import Dropdown from './Dropdown';
 import Image from 'next/image';
 import UseLoader from '@/store/loaderStore';
+import useThemeStore from '@/store/themeStore';
 
 const Navbar = () => {
   const router = useRouter();
   const { user, isAuthenticated, logout, checkAuth } = useAuthStore();
+    const { isDarkMode, toggleTheme } = useThemeStore();
   const pathname = usePathname();
-  const {showLoader} = UseLoader();
+  const { showLoader } = UseLoader();
 
   useEffect(() => {
-    checkAuth()
+    checkAuth();
   }, [checkAuth]);
 
   const loginKaro = () => {
@@ -31,20 +33,39 @@ const Navbar = () => {
   };
 
   const showNavBar = ["/", "/generate", "/login"].includes(pathname);
-
   if (!showNavBar) return null;
 
+  const Dark = "🌙"
+  const Light = "🌞"
+
   return (
-    <>
-      <div className="h-[100px] w-full flex items-center justify-center z-10">
-        <nav className="w-full justify-between h-[70px] shadow-gray-700 px-6 sm:px-10 flex items-center rounded-xl relative">
-          <Link href="/">
-            <h1 className="text-2xl sm:text-3xl NunitoEB font-bold text-slate-700 flex items-center justify-around">
-              <span className="text-cyan-700">&lt;</span>
-              A<Image src="Logo.svg" height={30} width={30} className='invert' alt='Logo' />L
-              <span className="text-cyan-700">&gt;</span>
-            </h1>
-          </Link>
+    <div className=' h-[100px] w-full flex items-center justify-center z-10 bg-transparent" '>
+
+      <nav className="w-full justify-between h-[70px] px-6 sm:px-10 flex items-center relative">
+
+        {/* Theme + Logo */}
+        <Link href="/">
+          <h1 className={` text-2xl sm:text-3xl NunitoEB font-bold ${isDarkMode ? 'text-white' : 'text-slate-700'} flex items-center justify-around `}>
+            <span className={`${isDarkMode ? 'text-white' : 'text-cyan-700'}`}>&lt;</span>
+            A<Image src="/Logo.svg" height={30} width={30} className={`${isDarkMode ? '' : 'invert'}`} alt='Logo' />L
+            <span className={`${isDarkMode ? 'text-white' : 'text-cyan-700'}`}>&gt;</span>
+          </h1>
+        </Link>
+
+        <div className='flex items-center justify-between gap-10 h-auto '>
+          {/* Theme toggle */}
+          <div className='flex flex-col items-center justify-center cursor-pointer'>
+            <button
+              onClick={toggleTheme}
+              type="button"
+              aria-label="toggle-theme"
+              className={`relative z-50 rounded-full w-14 h-7 ${isDarkMode ? 'bg-gray-200 shadow-white' : 'bg-gray-700 shadow-black'} flex items-center px-1 shadow-sm`}
+            >
+              <div className={`duration-300 transform transition-transform ${isDarkMode ? 'bg-gray-800' : 'bg-gray-200'} rounded-full w-5 h-5 ${isDarkMode ? "translate-x-7" : "translate-x-0"} flex items-center justify-center `}>
+                {isDarkMode ? Dark : Light}
+              </div>
+            </button>
+          </div>
 
           {isAuthenticated ? (
             <div className="NunitoEB text-green-500 flex items-center justify-center gap-3">
@@ -56,11 +77,12 @@ const Navbar = () => {
           ) : (
             <Button text="LogIn" click={loginKaro} />
           )}
-        </nav>
-      </div>
-    </>
-
+        </div>
+      </nav>
+    </div>
   );
 };
+
+Navbar.displayName = 'Navbar'
 
 export default Navbar;
